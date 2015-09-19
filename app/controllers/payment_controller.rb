160575bf6a8c74b0d95169ae1d2451b197cf31a4
306san <oh.taku528+git@gmail.com>
2015-09-20 04:03:01 +0900

    Paymentモデルの生成

---
+++ app/controllers/payment_controller.rb
@@ -0,0 +1,4 @@
+class PaymentController < ApplicationController
+  def create
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150919130721) do
+ActiveRecord::Schema.define(version: 20150919170413) do
+
+  create_table "money", force: :cascade do |t|
+    t.integer  "money",      limit: 4
+    t.datetime "created_at",           null: false
+    t.datetime "updated_at",           null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.integer  "UserID",     limit: 4
