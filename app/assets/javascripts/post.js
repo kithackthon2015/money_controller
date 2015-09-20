cba74d2620aa20dfe8b90e7a45ebae898e2d472c
306san <oh.taku528+git@gmail.com>
2015-09-20 12:11:31 +0900

    画像ファイルを追加

--- app/assets/javascripts/post.js
+++ app/assets/javascripts/post.js
@@ -1,24 +1,5 @@
-var arg = new Object;
-
-function showRemaining(){
-	var pair=location.search.substring(1).split('&');
-	for(var i=0;pair[i];i++) {
-		var kv = pair[i].split('=');
-		arg[kv[0]]=kv[1];
-	}
-
-	$("#showId").append(arg.Id);
-}
-
-var url;
-
-$(document).on("click","#sendRemaining",function(){
-	url = location.href;
-	if(url.indexOf('pulldown') == -1){
-		url += '&' + 'pulldown' + '=' + $("#posShow").val();
-	}
-
-	window.location = url;
-	// alert(arg.pulldown);
-
+$(document).ready(function() {
+setTimeout(function(){
+$('.odometer').html(parseInt($("#number").val())); //数字は最後の数値
+}, 1000);
 });
