8afce6fc733b1ca9b382d29d373688a17c9ad403
tyanogi <tyanogi@gmail.com>
2015-09-20 11:55:02 +0900

    DB周り修正。画像のリンク修正

--- db/seeds/user_dummy.rb
+++ db/seeds/user_dummy.rb
@@ -1,5 +1,5 @@
 User.create(:UserID => 10000, :target => 30000, :name => 'terada', :sex => 0, :salary =>500000, :picture => 'human.jpg')
 User.create(:UserID => 20000, :target => 20000, :name => 'yoda', :sex => 0, :salary =>100000, :picture => 'boy.jpg')
-User.create(:UserID => 30000, :target => 30000, :name => 'ogihara', :sex => 0, :salary =>300000, :picture => 'jibanyan.jpg')
-User.create(:UserID => 40000, :target => 10000, :name => 'ito', :sex => 0, :salary =>450000, :picture => 'and.jpg')
-User.create(:UserID => 50000, :target => 40000, :name => 'hirata', :sex => 1, :salary =>270000, :picture => 'cat.jpg')
+User.create(:UserID => 30000, :target => 30000, :name => 'ogihara', :sex => 0, :salary =>300000, :picture => 'jibanyan.png')
+User.create(:UserID => 40000, :target => 10000, :name => 'ito', :sex => 0, :salary =>450000, :picture => 'and.png')
+User.create(:UserID => 50000, :target => 40000, :name => 'hirata', :sex => 1, :salary =>270000, :picture => 'cat.png')
