588a1a645f27eff376c5579bc3e945b2a32b3b2a
306san <oh.taku528+git@gmail.com>
2015-09-20 10:23:14 +0900

    決済できるように

--- app/controllers/payment_controller.rb
+++ app/controllers/payment_controller.rb
@@ -1,12 +1,14 @@
 class PaymentController < ApplicationController
   def create
     @payment = Payment.new(payment_params)
+    @money = Money.find(@payment.user_id)
 if @payment.save
+    @money.money -= @payment.pay
+    @money.save
   render :action => 'new', notice: '追加しました'
 else
   render :action => 'new'
 end
-
   end
   def new
       @payment = Payment.new
